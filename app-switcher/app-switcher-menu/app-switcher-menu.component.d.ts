116867f346df55b659918a43ddab957181ecc7ac
Adam Nelson <adam@adamnel.com>
2020-04-01 19:47:38 +0000

    0797461 feat(app switcher): use scss + body class method for swapping logo (#81)

--- app-switcher/app-switcher-menu/app-switcher-menu.component.d.ts
+++ app-switcher/app-switcher-menu/app-switcher-menu.component.d.ts
@@ -1,10 +1,7 @@
-import { VantageThemeService } from '@td-vantage/ui-platform/theme';
 import { IVantageAppSwitcherItem } from '../services/products.service';
 export declare class VantageAppSwitcherMenuComponent {
-    _themeService: VantageThemeService;
     products: IVantageAppSwitcherItem[];
     otherProducts: IVantageAppSwitcherItem[];
     exploreMoreLink: string;
-    constructor(_themeService: VantageThemeService);
     _blockEvent(event: Event): void;
 }
